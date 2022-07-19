854d50b2dfdb49d05491af618b451cf58f91bf61
David <daecepi@gmail.com>
2022-07-19 13:04:13 -0500

    Adds README, package.json and the main tester file, to give the baseline for testing

---
+++ README.md
@@ -0,0 +1,10 @@
+# Hobby repository to play with modules again 
+This repo contains natively packaged libraries from NodeJS, explanations given asume that you have managed NodeJS projects in past
+
+## pre-requisites
+- NodeJS v17.1.0 and NPM >= 8.1.2
+
+## Usage
+- Run `npm install` (to include the local libraries)
+- Un-comment the code you want to test or add your own on the **file** tester.js
+- Run `npm start`
---
+++ tester.js
@@ -0,0 +1,20 @@
+// Testing clock
+// var SecondsWatcher = require('clock');
+// var seconds = 0;
+
+// var secondsWatcher = new SecondsWatcher();
+
+// secondsWatcher.once("second-passed", function(){
+//   console.log(`The clock has been started`)
+// });
+
+// secondsWatcher.on("second-passed", function(){
+//   seconds += 1;
+//   console.log(`Seconds that have passed: ${seconds}`)
+// })
+
+
+// Testing basic measurement library
+// const convertion = require('units-conversion');
+// const meters = 300;
+// console.log(`${convertion.feetToMeters(meters)} inches -> ${meters} meters`)
